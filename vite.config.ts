9b538f97ef7be775558e0cc229e69957c6e0cbd2
Andrew Colgin <acolgin@northeastern.edu>
2023-01-31 11:09:20 -0500

    timing plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+import timeReporter from "vite-plugin-time-reporter";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), timeReporter()],
   resolve: {
     alias: {
       // for TypeScript path alias import like : @/x/y/z
